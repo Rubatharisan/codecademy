3eda9818c3532005fb59592962a1de46beb0d4c3
Rubatharisan Thirumathyam <rubatharisan@gmail.com>
2015-10-22 18:00:33 +0200

    Added a few PHP challenges

---
+++ PHP/Introduction to PHP/PHP Syntax/Variables/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Variable Magic</title>
+	</head>
+	<body>
+        <!-- Add your PHP code between the <p></p> tags! -->
+        <p><?php $myFirstVariable = 0; $myName = "Rubas";?></p>
+    </body>
+</html>
---
+++ PHP/Introduction to PHP/Practicing with PHP/Echo It!/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link type='text/css' rel='stylesheet' href='style.css'/>
+		<title>PHP FTW!</title>
+	</head>
+	<body>
+        <!-- Write your PHP code below!-->
+        <p>
+            <?php
+                $myName = "Rubatharisan";
+                $myAge = 20;
+                echo $myName;
+                echo $myAge;
+            ?>
+        </p>   
+	</body>
+</html>
